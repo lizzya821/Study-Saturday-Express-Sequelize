498859a0a0ce9ba4785b1de917dbb87fb5efb2d1
lizzya821 <lizzya821@gmail.com>
2020-01-25 14:03:31 -0600

    completed studysaturday

--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,61 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bodyParser = require("body-parser");
+const students = require("../db/models/student.js");
 
+router.use(bodyParser.json());
+
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.get("/", async (req, res, next) => {
+  try {
+    res.send(await students.findAll());
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    let searchId = req.params["id"];
+    let foundStudent = await students.findOne({ where: { id: searchId } });
+    if (foundStudent) res.send(foundStudent);
+    else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  let newStudent = await students.create(req.body);
+  res.status(201).send(newStudent);
+});
+
+router.put("/:id", async (req, res, next) => {
+  try{
+    let searchId = req.params["id"];
+    let foundStudent = await students.findOne({ where: { id: searchId } });
+    let attToUpdate = Object.keys(req.body);
+    foundStudent[attToUpdate] = req.body[attToUpdate];
+    await foundStudent.save();
+    res.send(foundStudent);
+  }
+  catch(error){
+    next(error)
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try{
+    let searchId = req.params.id;
+    let foundStudent = await students.findOne({ where: { id: searchId } });
+    await foundStudent.destroy()
+    res.sendStatus(204)
+  }
+  catch(error){
+    next(error)
+  }
+});
 
 module.exports = router;
--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,48 @@
 const router = require('express').Router();
+const bodyParser = require("body-parser");
+const tests = require("../db/models/test.js");
 
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
+router.get("/", async (req, res, next) => {
+    try{
+        res.send(await tests.findAll())
+    }
+    catch(error){
+        next(error)
+    }
+})
+router.get("/:id", async (req, res, next)=> {
+    try {
+        let searchId = req.params["id"];
+        let foundTest = await tests.findOne({ where: { id: searchId } });
+        if (foundTest) res.send(foundTest);
+        else {
+          res.sendStatus(404);
+        }
+      } catch (error) {
+        next(error);
+      }
+})
+
+router.post("/student/:studentId", async(req, res, next) => {
+   let newTest =  await tests.create(req.body, {studentId: req.params.studentId})
+   newTest.studentId = Number(req.params.studentId)
+    await newTest.save()
+    res.status(201).send(newTest)
+    
+})
+
+router.delete("/:id", async (req, res, next) =>{
+    try{
+        let searchId = req.params.id
+        let foundTest = await tests.findOne({ where: { id: searchId } });
+        await foundTest.destroy()
+        res.sendStatus(204)
+    }
+    catch(error){
+        next(error)
+    }
+})
 module.exports = router;
